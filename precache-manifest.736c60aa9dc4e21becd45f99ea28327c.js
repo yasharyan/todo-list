a56400c441d67eb645261f9a79b226ba209c98aa
yasharyan <997yashraj@gmail.com>
2020-10-07 06:04:45 +0530

    Updates

---
+++ precache-manifest.736c60aa9dc4e21becd45f99ea28327c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ab434a80b189cb565f767d991e1bba",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "fe164725f0964a5d5b6c",
+    "url": "/todo-list/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5019de850b96e42f9dfc",
+    "url": "/todo-list/static/css/main.50e5e487.chunk.css"
+  },
+  {
+    "revision": "fe164725f0964a5d5b6c",
+    "url": "/todo-list/static/js/2.d65a9164.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.d65a9164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5019de850b96e42f9dfc",
+    "url": "/todo-list/static/js/main.8958f993.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
